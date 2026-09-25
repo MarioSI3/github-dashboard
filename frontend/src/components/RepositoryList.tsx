7f2a83ded6ae225c03a8235646f4eef30a0a7caf
MarioSI3 <mario.sierra.ta@gmail.com>
2026-09-25 16:54:42 +0200

    First Style for display

--- frontend/src/components/RepositoryList.tsx
+++ frontend/src/components/RepositoryList.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export function RepositoryList({ repositories }: Props) {
   return (
-    <div>   
+    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">   
       {repositories.map((repository) => (
         <RepositoryCard
           key={repository.url}
